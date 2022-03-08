7355deed3ef63f60b8242884a80e923751105dc8
anh s <jimmianh1807@gmail.com>
2022-03-08 23:48:28 +0700

    comit to git

---
+++ WebApplication6/Controllers/UsersController.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication6.Data;
+using WebApplication6.Models;
+
+namespace WebApplication6.Controllers
+{
+    public class UsersController : Controller
+    {
+        private Context db = new Context();
+
+        // GET: Users
+        private Context myIdentittyContext;
+        private UserManager<User> userManager;
+        public UsersController()
+        {
+            myIdentittyContext = new Context();
+            UserStore<User> userStore = new UserStore<User>(myIdentittyContext);
+            userManager = new UserManager<User>(userStore);
+        }
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(User user)
+        {
+            User userModel = new User()
+            {
+                UserName = user.UserName,
+                RoleNumber = user.RoleNumber,
+                PhoneNumber = user.PhoneNumber
+
+            };
+
+            var result = await userManager.CreateAsync(userModel, user.PasswordHash);
+            if (result.Succeeded)
+            {
+                return View("Successful");
+            }
+            else
+            {
+                return View("Failed");
+            }
+
+
+        }
+
+        public ActionResult Login()
+        {
+            return View(Login);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(string UserName, string PasswordHash)
+        {
+            var user = await userManager.FindAsync(UserName, PasswordHash);
+            if (user == null)
+            {
+
+                return View("LoginFailed");
+            }
+            else
+            {
+                SignInManager<User, string> signInManager
+                    = new SignInManager<User, string>(userManager, Request.GetOwinContext().Authentication);
+                await signInManager.SignInAsync(user, false, false);
+                return View("LoginSuccessful");
+            }
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,RoleNumber,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication6/Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication6.Models;
+
+namespace WebApplication6.Data
+{
+    public class Context : IdentityDbContext<User>
+    {
+        public Context() : base("NorthwindContex")
+        {
+        }
+    }
+}
